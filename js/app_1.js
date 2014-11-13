04a3fc5a303266c3013607fdf901ea2a067a8652
mgosnell <molly.gosnell@gmail.com>
2014-11-13 14:22:35 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'mgosnell.k7c209gp'; //<- this references the ugly green map that I made
+var mapId = 'mgosnell.k7c22agi'; //<- this references the ugly green map that I made
 var token = 'pk.eyJ1IjoibWdvc25lbGwiLCJhIjoiM1hBMTFZcyJ9.ESjOFERhirdz8HlSOBPADA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
